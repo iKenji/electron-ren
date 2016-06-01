3b0a6c1d26a62ddeaabb6cc1166ef70fd884de26
IchihashiKenji <ichihashi_kenji@infonear.co.jp>
2016-06-01 17:38:18 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// アプリケーションをコントロールするモジュール
+const {app} = require('electron');
+// ウィンドウを作成するモジュール
+const {BrowserWindow} = require('electron');
+
+// メインウィンドウはGCされないようにグローバル宣言
+var mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
