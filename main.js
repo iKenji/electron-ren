ed99979ecf8129a0d0cb19082283ef2e8ae5de11
ichihashi_kenji <jdabc1843@gmail.com>
2016-06-03 19:38:38 +0900

    quick start

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 'use strict';
 
+const electron = require('electron');
 // アプリケーションをコントロールするモジュール
-const {app} = require('electron');
+const {app} = electron;
 // ウィンドウを作成するモジュール
-const {BrowserWindow} = require('electron');
-
+const {BrowserWindow} = electron;
+const {Tray} = electron;
+const {Menu} = electron;
 // メインウィンドウはGCされないようにグローバル宣言
 var mainWindow = null;
-
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -18,11 +19,24 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+      'width': 800, 
+      'height': 600,
+      'transparent' : true,
+      'frame' : true
+  });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+  var appIcon = new Tray('images/icon.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'menu1' }
+  ])
+  appIcon.setToolTip('サンプル');
+  appIcon.setContextMenu(contextMenu);
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
 });
+
+
